78ccbc81d24ad8a4ed57d66a78bc670da2a82158
tokeh-hub <olatunjiaisha12@gmail.com>
2021-12-26 02:26:10 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
+import {FiSearch} from 'react-icons/fi'
+import axios from 'axios'
 import './App.css';
 
+// const api_key = '437e1e6990msha3f6ebee6ce3bdep190dc4jsn0d4e99b367c8'
+// const baseUrl = `https://veriphone.p.rapidapi.com/verify?key=${api_key}`
+// var axios = require("axios").default;
+
+
 function App() {
+  const [mobile,setMobile] = useState(null)
+  const [info,setInfo] = useState()
+  
+  var options = {
+    method: 'GET',
+    url: 'https://veriphone.p.rapidapi.com/verify',
+    params: {phone: `${mobile}`},
+    headers: {
+      'x-rapidapi-host': 'veriphone.p.rapidapi.com',
+      'x-rapidapi-key': '437e1e6990msha3f6ebee6ce3bdep190dc4jsn0d4e99b367c8'
+    }
+  };
+  const verify = () => {
+  axios.request(options).then(function (response) {
+    setInfo(response.data);
+    console.log(info)
+  }).catch(function (error) {
+    console.error(error);
+  });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='card'>
+         <div className='head'>
+         <input placeholder='search phone' onChange={e=>setMobile(e.target.value)}/>
+         <span className=''><FiSearch/></span>
+         <button className='btn' onClick={verify}  >Verify Phone Number</button>
+         </div>
+         {info && mobile?
+         <div className='info'>
+             <ul>
+               <li>Phone: <span>{info.phone}</span></li>
+               <li>Phone Validity: <span> {info.phone_valid?'true':'false'}</span></li>
+               <li>Country: <span>{info.country}</span></li>
+               <li>Country-Prefix: <span>{info.country_prefix}</span></li>
+               <li>Carrier: <span>{info.carrier}</span> </li>
+               <li>Country Code: <span>{info.country_code}</span></li>
+               <li>Phone Type: <span> {info.phone_type}</span></li>
+             </ul>
+         </div> : null}
+       </div>
     </div>
   );
 }
